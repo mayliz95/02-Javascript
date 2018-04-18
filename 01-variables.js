b84e274c936d6cf1b19f1a0129ec955f9b4df73b
mayliz95 <mayra.rosero@epn.edu.ec>
2018-04-18 15:09:07 -0500

    Definicion de algunas variables y pruebas con if

--- 01-variables.js
+++ 01-variables.js
@@ -1,1 +1,15 @@
 console.log("Hola Mundoo!");
+var nombre = "Mayra";
+var edad = 22;
+var deudas= 234.54;
+var fecha = new Date();
+var casado = false;
+var vacia = null;
+var noDefindo = undefined;
+
+console.log(vacia?"Verdadero":"Falso");  //Falso
+console.log(noDefindo?"Verdadero":"Falso"); //Falso
+console.log(0?"Verdadero":"Falso"); //Falso
+console.log(1?"Verdadero":"Falso"); //Verdadero
+console.log(-1?"Verdadero":"Falso"); //Verdadero
+
